d740c246d2185cfb0ccfa05a24193bf9e5ab426b
ASUS <khairunisaarsyad20@gmail.com>
2020-05-12 09:04:29 +0800

    LinearLayout_assesment 1

--- app/src/main/java/com/example/linearlayout/Main2Activity.java
+++ app/src/main/java/com/example/linearlayout/Main2Activity.java
@@ -1,10 +1,10 @@
 package com.example.linearlayout;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.LinearLayout;
 
 public class Main2Activity extends AppCompatActivity {
     LinearLayout btnlayout;
